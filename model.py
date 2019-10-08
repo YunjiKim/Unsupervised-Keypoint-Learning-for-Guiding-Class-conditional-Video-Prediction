d88f3c01d9311d5ee5b744b56eda301e2ecc209d
YunjiKim <yzeee1228@gmail.com>
2019-10-08 21:20:20 +0900

    Update model.py

--- model.py
+++ model.py
@@ -199,6 +199,7 @@ def Img_discriminator(self, x):
     logit = conv(x, channels=1, kernel=3, stride=1, pad=1, use_bias=False, scope='D_logit')
     return logit
 
+  
 class Vgg19:
   def __init__(self, vgg19_npy_path=None):
     if vgg19_npy_path is None:
@@ -461,6 +462,77 @@ class KD_IT(object):
     print('Avg. samples per second %.3f'%samples_per_sec)
 
   
+class EXT_KP(object):
+  def __init__(self, sess, config, global_step, training, dtype=tf.float32, name='EXT_KP'):
+    self.config = config.model
+    self.train_config = config.training
+    self._global_step = global_step
+    self.sess = sess
+    self.train_mode = training
+    
+  def build(self, inputs, output_tensors=False, build_loss=True):
+    im, idx, len_ = inputs['image'], inputs['idx'], inputs['len']
+    pts = self.pose_encoder(tf.reshape(im, [-1, 128,128,3]))
+    self.tensor = {'pts': tf.reshape(pts, [-1, 663, 40, 2]), 'idx': idx, 'len': len_, 'im':im}
+
+  def extract(self, opts, train_dataset, test_dataset, training_pl, handle_pl, checkpoint_fname, vars_to_restore=None, reset_global_step=True):
+   
+    tf.logging.set_verbosity(tf.logging.INFO)
+    # define iterators
+    train_iterator = train_dataset.make_initializable_iterator()
+    test_iterator = test_dataset.make_initializable_iterator()
+    global_init = tf.global_variables_initializer()
+    local_init = tf.local_variables_initializer()
+    self.sess.run([global_init,local_init])
+
+    # set up iterators
+    train_handle = self.sess.run(train_iterator.string_handle())
+    self.sess.run(train_iterator.initializer)
+    test_handle = self.sess.run(test_iterator.string_handle())
+
+    # restore checkpoint:
+    if tf.gfile.Exists(checkpoint_fname) or tf.gfile.Exists(checkpoint_fname + '.index'):
+      print('RESTORING MODEL from: ' + checkpoint_fname)
+      reader = tf.train.NewCheckpointReader(checkpoint_fname)
+      vars_to_restore = tf.global_variables()
+      checkpoint_vars = reader.get_variable_to_shape_map().keys()
+      vars_ignored = [
+          v.name for v in vars_to_restore if v.name[:-2] not in checkpoint_vars]
+      print(colorize('vars-IGNORED (not restoring):', 'blue', bold=True))
+      print(colorize(', '.join(vars_ignored), 'blue'))
+      vars_to_restore = [
+          v for v in vars_to_restore if v.name[:-2] in checkpoint_vars]
+      restorer = tf.train.Saver(var_list=vars_to_restore)
+      restorer.restore(self.sess, checkpoint_fname)
+    else:
+      raise Exception('model file does not exist at: ' + checkpoint_fname)
+
+    ########## compute output from trainset ##########
+    feed_dict = {handle_pl: train_handle, training_pl: False}
+    self.sess.run(train_iterator.initializer)
+    step = 0
+    while True:
+      try:
+        outputs = self.sess.run(self.tensor, feed_dict=feed_dict)
+        np.save('/workspace/imm/data/datasets/penn/unsup_pts_new/'+'{:04d}'.format(outputs['idx'][0])+'.npy',\
+               outputs['pts'][0,:outputs['len'][0],::])
+      except tf.errors.OutOfRangeError:
+        print('iteration through train set finished')
+        break
+
+    ########## compute output from testset ##########
+    feed_dict = {handle_pl: test_handle, training_pl: False}
+    self.sess.run(test_iterator.initializer)
+    while True:
+      try:
+        outputs = self.sess.run(self.tensor, feed_dict=feed_dict)
+        np.save('/workspace/imm/data/datasets/penn/unsup_pts_new/'+'{:04d}'.format(outputs['idx'][0])+'.npy',\
+               outputs['pts'][0,:outputs['len'][0],::])
+      except tf.errors.OutOfRangeError:
+        print('iteration through test set finished')
+        break
+  
+  
 class MOGEN(object):
   def __init__(self, sess, config, cell_info, vae_dim, global_step, training, dtype=tf.float32, name='MOGEN'):
     self.config = config.model
@@ -670,9 +742,9 @@ class MOGEN(object):
     print('Avg. samples per second %.3f'%samples_per_sec)
 
   
-class Evalutate(object):
+class EVAL(object):
 
-  def __init__(self, sess, config, cell_info, vae_dim, global_step, training, dtype=tf.float32, name='Evalutate'):
+  def __init__(self, sess, config, cell_info, vae_dim, global_step, training, dtype=tf.float32, name='EVAL'):
     self.config = config.model
     self.train_config = config.training
     self._global_step = global_step

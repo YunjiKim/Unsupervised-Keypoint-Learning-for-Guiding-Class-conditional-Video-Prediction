4a2d3af0f6397fc5f3628c3f7c3f2845f5d4ab43
yunjikim <yzeee1228@gmail.com>
2019-10-27 21:23:03 +0900

    Update prepare_penn_dataset.sh

--- prepare_penn_dataset.sh
+++ prepare_penn_dataset.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-original_data_dir=${1}
-output_data_dir=${2}
-
-mkdir -p ${output_data_dir}
-mv ${original_data_dir}/frames ${output_data_dir}/frames
-
-cp assets/penn_split/* ${output_data_dir}/
+data_dir=${1}
+cp assets/penn_split/* ${data_dir}/
